ad387a1c676d4d2ed7c363ba48f36f2a81ddb70c
ngyuki <ngyuki.ts@gmail.com>
2014-11-12 19:13:56 +0900

    Fix highlight

--- app/app.js
+++ app/app.js
@@ -5,8 +5,12 @@ var $marked = require('marked');
 var $highlight = require('highlight.js');
 
 $marked.setOptions({
-    highlight: function (code) {
-        return $highlight.highlightAuto(code).value;
+    highlight: function (code, lang) {
+        if (lang != null && $highlight.getLanguage(lang)) {
+            return $highlight.highlight(lang, code).value;
+        } else {
+            return $highlight.highlightAuto(code).value;
+        }
     }
 });
 
